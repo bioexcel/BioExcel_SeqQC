0c464575b12dc4880259ef5b0ac3c6499435ef53
Darren White <djwhiteastro@gmail.com>
2018-08-02 16:54:23 +0100

    Starting pep8 compatibility

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(
-    name='SeqQC',
+    name='SeqQC_BioExcel',
     version='0.1.0',
-    description=('Sequence Quality Control python module'),
+    description=('Sequence Quality Control python package'),
     author='Darren White',
     author_email='d.white@epcc.ed.ac.uk',
-    scripts=['bin/SeqQC'],
-    packages=['seq_qc'],
-    package_dir={'seq_qc': 'seq_qc'},
+    scripts=['bin/seqqc_bioexcel'],
+    packages=['seqqc_bioexcel'],
+    package_dir={'seqqc_bioexcel': 'seqqc_bioexcel'},
     install_requires=[],
 )
